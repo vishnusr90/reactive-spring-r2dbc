e7d1ac7b2f42b25aef3f6c5ba98f1ed8184b140f
Vishnu <vishnusr01@gmail.com>
2020-08-04 18:02:21 +0800

    Add basic files

---
+++ src/main/java/com/demo/reactive/ReactiveSpringR2dbcApplication.java
@@ -0,0 +1,13 @@
+package com.demo.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringR2dbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveSpringR2dbcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/reactive/ReactiveSpringR2dbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveSpringR2dbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
